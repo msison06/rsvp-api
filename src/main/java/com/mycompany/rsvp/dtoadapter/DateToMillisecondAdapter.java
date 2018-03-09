ff79ce0b5ce7954c00f7302c4058df06051520d3
Mark Anthony Sison <msison06@gmail.com>
2018-03-09 15:15:43 -0600

    feat(dto) date to millisecond json adapter
    JSON adapter for converting dates to milliseconds and vice versa.

---
+++ src/main/java/com/mycompany/rsvp/dtoadapter/DateToMillisecondAdapter.java
@@ -0,0 +1,21 @@
+package com.mycompany.rsvp.dtoadapter;
+
+import java.util.Date;
+import org.apache.johnzon.mapper.Adapter;
+
+/**
+ *
+ * @author Mark
+ */
+public class DateToMillisecondAdapter implements Adapter<Date, Long> {
+
+    @Override
+    public Date to(Long b) {
+        return new Date(b);
+    }
+
+    @Override
+    public Long from(Date a) {
+        return a.getTime();
+    }
+}

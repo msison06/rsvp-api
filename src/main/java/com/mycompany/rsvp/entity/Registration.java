59506c369a882cd4b0d49bec87619779a4f98222
Mark Anthony Sison <msison06@gmail.com>
2018-03-09 20:03:59 -0600

    feat(entity) registration table
    Adds registration table

---
+++ src/main/java/com/mycompany/rsvp/entity/Registration.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rsvp.entity;
+
+import com.mycompany.rsvp.dtoadapter.DateToMillisecondAdapter;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import org.apache.johnzon.mapper.JohnzonConverter;
+import org.apache.johnzon.mapper.JohnzonProperty;
+
+/**
+ *
+ * @author Mark
+ */
+@Entity
+@Table(name = "REGISTRATION")
+public class Registration implements Serializable {
+
+    @Id
+    private Long id;
+
+    @Column(name = "REG_DATE")
+    @JohnzonProperty("date")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @JohnzonConverter(DateToMillisecondAdapter.class)
+    private Date date;
+
+    @OneToOne
+    private RegisteredGuest guestInfo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public RegisteredGuest getGuestInfo() {
+        return guestInfo;
+    }
+
+    public void setGuestInfo(RegisteredGuest guestInfo) {
+        this.guestInfo = guestInfo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Registration other = (Registration) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+}

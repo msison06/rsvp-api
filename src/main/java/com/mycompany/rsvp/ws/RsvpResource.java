62ddc8a94ae71a9fd415e50cae2ca44dc76830b3
Mark Anthony Sison <msison06@gmail.com>
2018-03-09 23:51:58 -0600

    feat(rsvp) test webservices
    Implements test webservices. Incomplete, will continue work later.

--- src/main/java/com/mycompany/rsvp/ws/RsvpResource.java
+++ src/main/java/com/mycompany/rsvp/ws/RsvpResource.java
@@ -1,8 +1,16 @@
 package com.mycompany.rsvp.ws;
 
-import com.mycompany.rsvp.entity.Guest;
+import com.mycompany.rsvp.entity.Event;
+import com.mycompany.rsvp.entity.Registration;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,16 +25,61 @@ import javax.ws.rs.core.Response;
 @Path("")
 public class RsvpResource {
 
+    @PersistenceUnit(name = "rsvp_pu")
+    EntityManagerFactory emf;
+
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/info/id/{id}")
     public Response eventInfo(@PathParam("id") Long id) {
-        return Response.ok().build();
+
+        EntityManager em = emf.createEntityManager();
+
+        Event event = em.find(Event.class, id);
+
+        return Response.ok(event).build();
     }
 
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/register")
-    public Response register(Guest guest) {
-        return Response.ok().build();
+    public Response register(Registration registration) {
+        EntityManager em = emf.createEntityManager();
+
+        em.persist(registration.getGuestInfo());
+
+        Long eventId = registration.getEvent().getId();
+        Event event = em.find(Event.class, eventId);
+        registration.setEvent(event);
+
+        em.persist(registration);
+
+        return Response.ok(registration).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/createEvent")
+    public Response register(Event event) {
+        EntityManager em = emf.createEntityManager();
+
+        em.persist(event);
+
+        return Response.ok(event).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/test")
+    public Response test() {
+
+        EntityManager em = emf.createEntityManager();
+
+        Query q = em.createQuery("select e from Event as e");
+        List<Event> events = q.getResultList();
+
+        return Response.ok(events.toArray()).build();
     }
 }

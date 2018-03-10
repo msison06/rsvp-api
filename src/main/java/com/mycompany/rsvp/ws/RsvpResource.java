dc0028e2ceecd6fe5438ee8feb143167598896b7
Mark Anthony Sison <msison06@gmail.com>
2018-03-09 22:46:42 -0600

    feat(rsvp) rsvp endpoint stubs
    Stubs out endpoints for registering and getting event info.

--- src/main/java/com/mycompany/rsvp/ws/RsvpResource.java
+++ src/main/java/com/mycompany/rsvp/ws/RsvpResource.java
@@ -1,7 +1,13 @@
 package com.mycompany.rsvp.ws;
 
+import com.mycompany.rsvp.entity.Guest;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -11,4 +17,16 @@ import javax.ws.rs.Path;
 @Path("")
 public class RsvpResource {
 
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/info/id/{id}")
+    public Response eventInfo(@PathParam("id") Long id) {
+        return Response.ok().build();
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/register")
+    public Response register(Guest guest) {
+        return Response.ok().build();
+    }
 }
